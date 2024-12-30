111b6d27770abedcf638231d5681c93c8759227e
Chaitanya Allu <chaitanyaa1999@gmail.com>
2024-12-29 23:46:21 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "Hello World from H1!"),
+    React.createElement("h1", { id: "heading" }, "Hello World from H2!"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
