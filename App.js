07202e8d0ddaeb42e9cd4bcb4894cde1b82d464f
Chaitanya Allu <chaitanyaa1999@gmail.com>
2024-12-31 14:54:36 -0500

    ep-01,02,03

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Hello World from H1!"),
-    React.createElement("h1", { id: "heading" }, "Hello World from H2!"),
-  ])
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", { id: "heading" }, "Hello World from H1!"),
+//     React.createElement("h1", { id: "heading" }, "Hello World from H2!"),
+//   ])
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+// NOTE: react.createElement => JS Object => HTML Element when rendered on DOM
+// const heading = React.createElement("h1", {}, "Hello Heme! 🤬");
+
+// NOTE: JSX => React.createElement => JS Object => HTML Element when rendered on DOM
+// const jsxHeading = <h1>Hello Heme! 🤬 - JSX</h1>;
+
+const randomNum = 10;
+
+const Title = () => {
+  return <h1>Hello,</h1>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title/>
+      <h2> This is a heading functional component! </h2>
+      {console.log(randomNum + 20)}
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
---
+++ README.md
@@ -0,0 +1,20 @@
+# React Learning
+
+## Parcel
+- Builds Dev
+- Provides Local Server
+- Uses HMR - Hot Module Replacement -> which checks and applies updates
+- File Watching Algo - search for file changes
+- Caches. Hence, builds faster
+- Image Optimisation
+- Minification
+- Bundling
+- Compressing
+- Uses Consistent Hashing
+- Code Splitting
+- Differential Bundling -> Supports older browsers aswell by providing different builds for different browsers
+- Diagnostics
+- Better Error Handling
+- Provides HTTPS
+- Tree Shaking Algorithm - Removes unused code
+- Different build for dev and production
